6170083700e985abbd61cc3b1d47b20b0da695d1
sumond25 <sumond936@gmail.com>
2021-11-03 14:17:16 +0530

    ADD LIBRARY CODES

--- projects/ngx-geo-autocomplete/src/lib/ngx-geo-autocomplete.module.ts
+++ projects/ngx-geo-autocomplete/src/lib/ngx-geo-autocomplete.module.ts
@@ -1,12 +1,38 @@
-import { NgModule } from '@angular/core';
-import { NgxGeoAutocompleteComponent } from './ngx-geo-autocomplete.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgxGeoAutocompleteComponent } from './auto-complete.component';
+import { AutoCompleteSearchService } from './auto-complete.service';
+import { LocalStorageService } from './storage.service';
+import { BrowserGlobalRef, GlobalRef } from './windowRef.service';
 
 
 
 @NgModule({
-  declarations: [NgxGeoAutocompleteComponent],
-  imports: [
+  declarations: [
+    NgxGeoAutocompleteComponent
   ],
-  exports: [NgxGeoAutocompleteComponent]
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  exports: [
+    NgxGeoAutocompleteComponent
+  ],
+  providers : [
+    { 
+      provide: GlobalRef, 
+      useClass: BrowserGlobalRef 
+    },
+    AutoCompleteSearchService, 
+    LocalStorageService]
 })
-export class NgxGeoAutocompleteModule { }
+export class NgxGeoAutocompleteModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgxGeoAutocompleteModule
+    };
+  }
+}
