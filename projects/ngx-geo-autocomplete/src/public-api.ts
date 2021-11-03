2cc962b8d4aea779d41fe3c1193764999be4d26f
sumond25 <sumond936@gmail.com>
2021-11-03 21:07:18 +0530

    public_api fix

--- projects/ngx-geo-autocomplete/src/public-api.ts
+++ projects/ngx-geo-autocomplete/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-geo-autocomplete
  */
 
-export * from './lib/ngx-geo-autocomplete.service';
-export * from './lib/ngx-geo-autocomplete.component';
+export * from './lib/auto-complete.component';
 export * from './lib/ngx-geo-autocomplete.module';
